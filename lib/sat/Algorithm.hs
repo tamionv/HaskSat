50f90f94d69e0ca5f5739f7d0108157fb3baea37
tamionv <tamionv@gmail.com>
2020-05-04 10:36:52 +0300

    Algorithm refactor
    `AlgState` is a record. Also it is only constructed with `derivedState`
    and 'initialState` within the algorithm. Thus to extend it, just add extra
    fields, mainting invariants in these constructors.

--- lib/sat/Algorithm.hs
+++ lib/sat/Algorithm.hs
@@ -8,17 +8,24 @@ import Sat.Propositional
 import Data.List
 import Data.Ord
 
-data AlgState = Initial Formula
-                  | Derived Lit Formula (Maybe [Lit]) AlgState
-                  deriving (Show, Eq, Ord)
+data AlgState = Initial { headForm :: Formula }
+                | Derived { headForm :: Formula
+                          , decision :: Lit
+                          , reason :: Maybe [Lit]
+                          , previousState :: AlgState
+                          }
+                deriving (Show, Eq, Ord)
+
+initialState :: Formula -> AlgState
+initialState x = Initial { headForm = x }
 
 derivedState :: Lit -> Maybe [Lit] -> AlgState -> AlgState
-derivedState l r fs = Derived l (setInFormula l (headForm fs)) r fs
+derivedState l r fs = Derived { headForm = setInFormula l (headForm fs)
+                              , decision = l
+                              , reason = r
+                              , previousState = fs
+                              }
 
-headForm :: AlgState -> Formula
-headForm fs = case fs of
-    Initial f -> f
-    Derived _ f _ _ -> f
 
 unitPropagation :: AlgState -> AlgState
 unitPropagation fs = derivedState lit (Just r) fs where
@@ -30,28 +37,29 @@ learnedClause :: AlgState -> Clause
 learnedClause fs = go c fs where
     c = refreshClause $ head $ filter ((==0) . clauseSize ) $ headForm fs
     go c fs = case fs of
-        Derived _ _ Nothing fs' -> go c fs'
-        Derived l _ (Just r) fs'
+        Derived { reason = Nothing } -> go c $ previousState fs
+        Derived { decision = l, reason = Just r, previousState = fs' }
             | (-l) `inClause` c -> go (foldr addLiteral (removeLiteral (-l) c) r) fs'
             | otherwise -> go c fs'
         _ -> c
 
 addClause :: Clause -> AlgState -> AlgState
 addClause c fs = fst $ go c fs where
-    go c (Initial f) = (Initial (c:f), Just c)
-    go c (Derived l f r fs) = (Derived l f' r fs', c'') where
-        (fs', c') = go c fs
-        c'' = c' >>= setInClause l
-        f' = case c'' of
-            Just x -> x:f
-            _ -> f
+    go c (Initial { headForm = f } ) = (initialState (c:f), Just c)
+    go c fs@Derived{ headForm=f, decision=l, previousState=fs' }
+        = (derivedState l (reason fs) fs'', c'') where
+            (fs'', c') = go c fs'
+            c'' = c' >>= setInClause l
+            f' = case c'' of
+                Just x -> x:headForm fs'
+                _ -> f
 
 aLit :: AlgState -> Lit
 aLit = aClauseLit . minimumBy (comparing $ length . current) . headForm
 
 choices :: AlgState -> [Lit]
 choices fs = case fs of
-    Derived l _ _ fs' -> l:choices fs'
+    Derived { decision=l, previousState=fs' } -> l:choices fs'
     _ -> []
 
 algorithmAction :: StateT AlgState (Either Clause) [Lit]
@@ -79,6 +87,6 @@ algorithmAction = do go ; gets choices where
     choose lit = do modify $ derivedState lit Nothing ; go
 
 findSat :: Formula -> Maybe [Lit]
-findSat f = case runStateT algorithmAction $ Initial f of
+findSat f = case runStateT algorithmAction $ initialState f of
     Right (x, _) -> Just x
     _ -> Nothing
